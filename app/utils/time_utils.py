355e30b372b2ffa70f1099fa898751c110da6868
A-krt <ardahan.kartaloglu@gmail.com>
2026-05-31 12:24:09 +0200

    Add timezone utility functions for timestamps

--- app/utils/time_utils.py
+++ app/utils/time_utils.py
@@ -1,1 +1,12 @@
+from __future__ import annotations
 
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+def now_in_timezone(timezone_name: str) -> datetime:
+    return datetime.now(ZoneInfo(timezone_name))
+
+
+def timestamp_for_filename(timezone_name: str) -> str:
+    return now_in_timezone(timezone_name).strftime("%Y-%m-%d_%H-%M-%S")
