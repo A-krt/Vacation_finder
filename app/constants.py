12be953b401db2c08d879f089843a1ac1a6fb32f
A-krt <ardahan.kartaloglu@gmail.com>
2026-05-31 12:18:51 +0200

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,1 +1,6 @@
+from __future__ import annotations
 
+ALLOWED_ACCOMMODATION_TYPES = {"hotel", "apartment", "holiday_home"}
+ALLOWED_TAGS = {"beach", "city", "restaurants", "bars"}
+QUALITY_SCORE_MIN = 8.0
+DEFAULT_CURRENCY = "EUR"
